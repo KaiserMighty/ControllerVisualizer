b4d3dcff93c1831b7f1da51ced7b51c8560ebabc
Kaiser Mighty <unikmey@gmail.com>
2024-05-12 22:38:01 -0700

    File Upload

---
+++ InnerStick.gd
@@ -0,0 +1,8 @@
+extends ColorRect
+
+func _process(delta):
+	var max_x = get_parent().size[0] - size[0]
+	var max_y = get_parent().size[1] - size[1]
+	var cur_x = (Input.get_joy_axis(1, JOY_AXIS_LEFT_X) + 1) * max_x/2
+	var cur_y = (Input.get_joy_axis(1, JOY_AXIS_LEFT_Y) + 1) * max_y/2
+	set_position(Vector2(cur_x, cur_y))
